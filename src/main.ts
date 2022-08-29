1d57772d5392fc802c1eb6e3b8720a082588ea8b
elton11220 <jonesbak@outlook.com>
2022-08-29 08:23:54 +0800

    chore: removed amfe-flexible and postcss-pxtorem

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import "amfe-flexible";
 
 createApp(App).use(router).mount("#app");
