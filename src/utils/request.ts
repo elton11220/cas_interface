c6b0cf3a0fb614f734fed6e94eb7e9ef564a35d0
elton11220 <jonesbak@outlook.com>
2022-09-01 12:43:14 +0800

    feat: packaged Axios instance

---
+++ src/utils/request.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosRequestConfig } from "axios";
+import type { AxiosResponse, AxiosError } from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: '/api',
+  timeout: 1000 * 20,
+  withCredentials: true,
+});
+
+interface ApiResult<T = any> {
+  code: number;
+  msg: string;
+  data?: T;
+}
+
+interface ErrResult {
+  code: number;
+  msg: string;
+}
+
+axiosInstance.interceptors.response.use(
+  (res) => {
+    const code = res.data.code;
+    const message = res.data.msg;
+    if (code === 200 || code === 201) {
+      window.$message.success(message);
+    }
+    return res.data;
+  },
+  (error: AxiosError<ErrResult>) => {
+    const code = error.response?.status || 400;
+    const message = error.response?.data.msg || error.message || '请求失败';
+    if (code === 400 || code === 403) {
+      window.$message.error(message);
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default <T = null> (config: AxiosRequestConfig) => {
+  return new Promise<ApiResult<T>>((resolve, reject) => {
+    axiosInstance.request<ApiResult<T>>(config).then((response: AxiosResponse<ApiResult<T>>) => {
+      resolve(response.data)
+    }).catch((error :any) => {
+      reject(error)
+    })
+  })
+}
